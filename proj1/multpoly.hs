5ff66bc5bc13d3d7768d6bd715b013a3115792d2
filipecardoso0 <filipe12200@gmail.com>
2022-10-20 18:35:49 +0100

    Added Polynomial Multiplication

--- proj1/multpoly.hs
+++ proj1/multpoly.hs
@@ -2,4 +2,29 @@ module MultPoly where
 import Normalize
 import Parsing
 
-multPoly::[Polynomial]->Polynomial
+p1 :: Polynomial
+p1 = [(-12,[('x',2),('y',2)])]
+p2 :: Polynomial
+p2 = [(-4,[('x',2),('y',1)])]
+
+multTerm :: Term -> Term -> Term
+multTerm t1 t2 = (coef, newvars)
+    where
+        coef = fst t1 * fst t2
+        newvars = snd t1 ++ snd t2
+
+mult2Poly :: Polynomial -> Polynomial -> Polynomial
+mult2Poly poly1 [] = poly1
+mult2Poly [] poly2 = poly2
+mult2Poly poly1 poly2 = [multTerm x y | x<-poly1, y<-poly2] 
+
+multPoly :: [Polynomial] -> Polynomial
+multPoly [[]] = []
+multPoly [x] = x
+multPoly (x:y:[]) = mult2Poly x y
+multPoly (x:y:xs) =  (mult2Poly x y) ++ multPoly (xs)
+
+
+t = multPoly [[(3, [('x',1),('y',1)]), (1,[('x',1)])]]
+res = normalizePoly $ multPoly [[(3, [('x',1),('y',1)]), (1,[('x',1)])] , [(-4, [('x',1),('y',1)])]]
+res1 = normalizePoly $ multPoly [[(3, [('x',1),('y',1)]), (1,[('x',1)])] , [(-4, [('x',1),('y',1)]), (-10, [('x',4),('y',3)])]]
