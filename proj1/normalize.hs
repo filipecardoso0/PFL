de15cef61875b3b4d65f773731aba780374a9c81
PedroOliveira021 <94188004+PedroOliveira021@users.noreply.github.com>
2022-10-23 23:54:05 +0100

    Update normalize.hs

--- proj1/normalize.hs
+++ proj1/normalize.hs
@@ -2,21 +2,22 @@ module Normalize where
 import Parsing
 import InsertionSort
 import Data.List
-
+import Data.Char (ord)
+--Funcao para remover aquando do parse os coeficientes que sao iguais a 0
 removeCoef0 :: Polynomial -> Polynomial
 removeCoef0 [] = []
 removeCoef0 (x:xs) 
     | fst x == 0 = removeCoef0 xs
     | otherwise = x : removeCoef0 xs
 
-
+--Funcao para remover aquando do parse os expoentes que sao iguais a 0
 removeExp0  :: TupleList -> TupleList
 removeExp0 [] = []
 removeExp0 (x:xs) 
   | snd x  == 0  = removeExp0 xs
   | otherwise = x : removeExp0 xs
 
-
+--Funcao para  quando numa lista de tuplos tem a mesma variavel soma-se os expoentes 
 addSameVariable :: TupleList -> TupleList
 addSameVariable [] = []
 addSameVariable [x] = [x]
@@ -25,7 +26,7 @@ addSameVariable (x:y:xs)
  | otherwise = x : addSameVariable (y:xs)
   where n = (fst x, snd x + snd y)
 
-
+--funcao que soma os coeficientes quando os termos sao iguais
 simplifyPoly :: Polynomial -> Polynomial
 simplifyPoly [] = []
 simplifyPoly [x] = [x]
@@ -35,14 +36,14 @@ simplifyPoly (x:y:xs)
     where
         n = (fst x + fst y, snd x)
 
-
+--funcao que compara a lista de tuplos de 2 termos diferentes
 mycompare :: Term -> Term -> Ordering
 mycompare a b 
   | snd a > snd b = GT
   | snd a == snd b = EQ
   | otherwise = LT
 
-
+--funcao que usa todas as funcoes descritas acima para normalizar o polinomio
 normalizePoly :: Polynomial -> Polynomial
 normalizePoly [] = []
-normalizePoly poly = reverse $ removeCoef0 (simplifyPoly (sortBy mycompare ([(fst x,addSameVariable (myisort((removeExp0 (snd x)))))  | x<-poly])))
+normalizePoly poly =  removeCoef0 (simplifyPoly (sortBy mycompare ([(fst x, addSameVariable ( orderTupleList (myisort  (removeExp0 (snd x)))))  | x<-poly])))
