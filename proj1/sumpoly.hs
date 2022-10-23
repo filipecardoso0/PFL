7d644976c3b94c3e4582eefa1a6a3a322ddf1429
PedroOliveira021 <94188004+PedroOliveira021@users.noreply.github.com>
2022-10-23 23:55:00 +0100

    Update sumpoly.hs

--- proj1/sumpoly.hs
+++ proj1/sumpoly.hs
@@ -2,7 +2,7 @@ module SumPoly where
 import Normalize
 import Parsing
 
-
+--Funcao que normaliza a concatenacao dos n termos passados
 sumPoly :: [Polynomial] -> Polynomial
 sumPoly [] = []
 sumPoly (x:xs) = normalizePoly (x ++ sumPoly (xs))
