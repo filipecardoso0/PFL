d1adc58813620ad7cef2fe6746f9b48a99e7e2f9
filipecardoso0 <filipe12200@gmail.com>
2022-12-31 03:10:35 +0000

    Jumps Menu Popup done

--- proj2/teste.pl
+++ proj2/teste.pl
@@ -42,37 +42,52 @@ write_separator :-
     write('---+---+---+---').
 
 
+/* TODO TENTAR COLOCAR AVISOS AO INVES DE EM CASO DE ERRO DETETADO OBRIGAR O JOGADOR A REINCIAR O JOGO */ 
+% ------------------- Player Turn Menu -----------------------
+
+% Jumps are mandatory and take precedence over drops and moves.
+% Multiple jumps, if possible, are required (no max capture rule)
+
 
 % ------------------- Player Turn Menu -----------------------
 
-/* TODO TENTAR COLOCAR AVISOS AO INVES DE EM CASO DE ERRO DETETADO OBRIGAR O JOGADOR A REINCIAR O JOGO */ 
+player_turn(Elements, CurrPlayer, LastPlayedIndex, Player1Stones, Player2Stones):-
+    /* DEBUG INFO
+    write('Player 1 Stones '), write(Player1Stones), nl,
+    write('Player 2 Stones '), write(Player2Stones), nl,
+    write('Last Played Index: '), write(LastPlayedIndex), nl,
+    */
+    if(verify_inserted_nearby_player(CurrPlayer, LastPlayedIndex, Player1Stones, Player2Stones), player_turn_jump(Elements, CurrPlayer, Player1Stones, Player2Stones), player_turn_DropOrMove(Elements, CurrPlayer, Player1Stones, Player2Stones)).
 
-player_turn(Elements, CurrPlayer):-
+
+player_turn_jump(Elements, CurrPlayer, Player1Stones, Player2Stones):-
     write('Player: '), write(CurrPlayer), nl,
+    write('Please select an action:'), nl,
+    write('1- Jump over an enemy stone.'), nl,
+    read(Choice),
+    (
+        Choice = 1 -> jump_over_stone(Elements, CurrPlayer, Player1Stones, Player2Stones)
+    ).
+
+player_turn_DropOrMove(Elements, CurrPlayer, Player1Stones, Player2Stones):-
+    write('Player: '), 
+    write(CurrPlayer), nl,
     write('Please select an action:'), nl,
     write('1- Drop a stone into an empty cell.'), nl,
     write('2- Move a stone into an adjacent(orthogonal or diagonal empty cell.'), nl,
 
-    /* TODO ONLY LET PLAYER SELECT CHOICE 2 IF HE ALREADY HAS 1 STONE ON THE BOARD */
-    /* TODO Option 3 - Jump over an enemy stone landing on the immediate empty cell, and capturing it; 
-        	            - Jumps are mandatory and take precedence over drops and moves. 
-                        - Multiple jumps, if possible, are required (no max capture rule) 
-
-        TODO WIN - Wins the player that makes an orthogonal or diagonal 3 in-a-row, or captures 6 enemy stones.
-    */
-
     read(Choice),
     (
-        Choice = 1 -> drop_stone_menu(Elements, CurrPlayer); 
-        Choice = 2 -> move_stone(Elements, CurrPlayer)
+        Choice = 1 -> drop_stone_menu(Elements, CurrPlayer, Player1Stones, Player2Stones); 
+        Choice = 2 -> move_stone(Elements, CurrPlayer, Player1Stones, Player2Stones)
     ).
 
+% ------------------- Jump Over Enemy Stone -----------------------
+jump_over_stone(Elements, CurrPlayer):- write('Saltar Pedra').
 
 % ------------------- Move Stone -----------------------
 
-/* NOTA (Remover depois): O board tem que ser indo passado a cada passo, pois é como se fosse um state pattern e corresponde ao estado atual do jogo */
-
-move_stone(Elements, CurrPlayer):-
+move_stone(Elements, CurrPlayer, Player1Stones, Player2Stones):-
     write('Select which Stone do you want to move:'), nl, 
     write('Select the column where the stone is located (1-4):'), nl, 
     read(Col), 
@@ -83,25 +98,26 @@ move_stone(Elements, CurrPlayer):-
    Position is (Row-1)*4+Col,
    PositionIndex is Position-1, 
  
-   /* Verify if the stone belongs to the player - TODO WHEN PLAYER VS PLAYER STARTS TO BE IMPLEMENTED  
-   belongs_to_player(Elements, PositionIndex, player), */ 
+   /* TODO - Verify if the stone belongs to the player 
+   if(belongs_to_player(CurrPlayer, PositionIndex, Player1Stones, Player2Stones) ,move_stone(Elements, CurrPlayer, Player1Stones, Player2Stones)), 
+    */
 
    /* Select the movement type */ 
-   movement_selector(Elements, PositionIndex, CurrPlayer).
+   movement_selector(Elements, PositionIndex, CurrPlayer, Player1Stones, Player2Stones).
 
 /* Move a stone into an adjacent(orthogonal or diagonal empty cell */
-movement_selector(Elements, PositionIndex, CurrPlayer):-
+movement_selector(Elements, PositionIndex, CurrPlayer, Player1Stones, Player2Stones):-
     write('Please Select the movement Type: '), nl,
     write('1- Orthogonal'), nl, 
     write('2- Diagonal'), nl, 
     read(Type),
     (
-        Type = 1 -> show_options_orthogonal(Elements, PositionIndex, CurrPlayer); 
-        Type = 2 -> move_diagonal(Elements, PositionIndex, CurrPlayer)
+        Type = 1 -> show_options_orthogonal(Elements, PositionIndex, CurrPlayer, Player1Stones, Player2Stones); 
+        Type = 2 -> show_options_diagonal(Elements, PositionIndex, CurrPlayer, Player1Stones, Player2Stones)
     ).
 
 /* Move a stone orthogonally */
-show_options_orthogonal(Elements, PositionIndex, CurrPlayer):-
+show_options_orthogonal(Elements, PositionIndex, CurrPlayer, Player1Stones, Player2Stones):-
    write('Please select the direction of the movement'), nl, 
    write('1- Right'), nl, 
    write('2- Left'), nl, 
@@ -109,15 +125,15 @@ show_options_orthogonal(Elements, PositionIndex, CurrPlayer):-
    write('4- Down'), nl, 
    read(Type), 
    (
-        Type = 1 -> move_orthogonal(Elements, PositionIndex, 1, CurrPlayer); 
-        Type = 2 -> move_orthogonal(Elements, PositionIndex, -1, CurrPlayer); 
-        Type = 3 -> move_orthogonal(Elements, PositionIndex, 4, CurrPlayer); 
-        Type = 4 -> move_orthogonal(Elements, PositionIndex, -4, CurrPlayer)
+        Type = 1 -> move_orthogonal(Elements, PositionIndex, 1, CurrPlayer, Player1Stones, Player2Stones); 
+        Type = 2 -> move_orthogonal(Elements, PositionIndex, -1, CurrPlayer, Player1Stones, Player2Stones); 
+        Type = 3 -> move_orthogonal(Elements, PositionIndex, 4, CurrPlayer, Player1Stones, Player2Stones); 
+        Type = 4 -> move_orthogonal(Elements, PositionIndex, -4, CurrPlayer, Player1Stones, Player2Stones)
    ). 
 
 
 /* Move a stone diagonally */
-move_diagonal(Elements, PositionIndex, CurrPlayer):-
+show_options_diagonal(Elements, PositionIndex, CurrPlayer, Player1Stones, Player2Stones):-
    write('Please select the direction of the movement'), nl, 
    write('1- Right Up'), nl, 
    write('2- Left Up'), nl, 
@@ -125,30 +141,30 @@ move_diagonal(Elements, PositionIndex, CurrPlayer):-
    write('4- Left Down'), nl, 
    read(Type), 
    (
-        Type = 1 -> move_diagonally(Elements, PositionIndex, -3, CurrPlayer); 
-        Type = 2 -> move_diagonally(Elements, PositionIndex, -5, CurrPlayer); 
-        Type = 3 -> move_diagonally(Elements, PositionIndex, 5, CurrPlayer); 
-        Type = 4 -> move_diagonally(Elements, PositionIndex, 3, CurrPlayer)
+        Type = 1 -> move_diagonally(Elements, PositionIndex, -3, CurrPlayer, Player1Stones, Player2Stones); 
+        Type = 2 -> move_diagonally(Elements, PositionIndex, -5, CurrPlayer, Player1Stones, Player2Stones); 
+        Type = 3 -> move_diagonally(Elements, PositionIndex, 5, CurrPlayer, Player1Stones, Player2Stones); 
+        Type = 4 -> move_diagonally(Elements, PositionIndex, 3, CurrPlayer, Player1Stones, Player2Stones)
    ). 
 
-move_diagonally(Elements, PositionIndex, Accomulator, CurrPlayer):-
+move_diagonally(Elements, PositionIndex, Accomulator, CurrPlayer, Player1Stones, Player2Stones):-
     NewPositionIndex is PositionIndex+Accomulator, 
 
     /* If the stone is on the first or last column it cannot move to the left(1st) or to the right(last) */
     can_move_diagonally(PositionIndex, Accomulator),
 
-    move_general(Elements, PositionIndex, NewPositionIndex, CurrPlayer).
+    move_general(Elements, PositionIndex, NewPositionIndex, CurrPlayer, Player1Stones, Player2Stones).
 
-move_orthogonal(Elements, PositionIndex, Accomulator, CurrPlayer):-
+move_orthogonal(Elements, PositionIndex, Accomulator, CurrPlayer, Player1Stones, Player2Stones):-
     NewPositionIndex is PositionIndex+Accomulator,
 
     /* Verify if the New Position is Adjacent to the Previous position */
     verify_adjacent(PositionIndex, NewPositionIndex),
 
-    move_general(Elements, PositionIndex, NewPositionIndex, CurrPlayer).
+    move_general(Elements, PositionIndex, NewPositionIndex, CurrPlayer, Player1Stones, Player2Stones).
 
 
-move_general(Elements, PositionIndex, NewPositionIndex, CurrPlayer):-
+move_general(Elements, PositionIndex, NewPositionIndex, 1, Player1Stones, Player2Stones):-
 
     /* Verify if the next position index is valid */
     is_next_position_valid(NewPositionIndex), 
@@ -158,32 +174,44 @@ move_general(Elements, PositionIndex, NewPositionIndex, CurrPlayer):-
 
     /* Remove the stone from the current position */ 
     replace(Elements,PositionIndex,0,NewElements),
+    /* Update Player Stones */ 
+    replace(Player1Stones, PositionIndex, 0, NewPlayer1Stones),
 
-    move_player(NewElements, NewPositionIndex, CurrPlayer).
+    move_player(NewElements, NewPositionIndex, CurrPlayer, NewPlayer1Stones, Player2Stones).
 
-move_player(Elements, NewPositionIndex, 1):-
+move_general(Elements, PositionIndex, NewPositionIndex, 1, Player1Stones, Player2Stones):-
+
+    /* Verify if the next position index is valid */
+    is_next_position_valid(NewPositionIndex), 
+
+    /* Verify if the position where we are going to go next is taken */ 
+    not_taken_rec(Elements, NewPositionIndex), 
+
+    /* Remove the stone from the current position */ 
+    replace(Elements,PositionIndex,0,NewElements),
+    /* Update Player Stones */ 
+    replace(Player2Stones, PositionIndex, 0, NewPlayer2Stones),
+
+    move_player(NewElements, NewPositionIndex, CurrPlayer, Player1Stones, NewPlayer2Stones).
+
+move_player(Elements, NewPositionIndex, 1, Player1Stones, Player2Stones):-
     /* Move the stone to its new position */ 
     replace(Elements, NewPositionIndex, p1, NewElements),
+    /* Update Player 1 Stones */ 
+    replace(Player1Stones, NewPositionIndex, p1, NewPlayer1Stones),
 
-    finalize_move(NewElements, CurrPlayer).
+    finish_player_turn(NewElements, CurrPlayer, NewPositionIndex, NewPlayer1Stones, Player2Stones).
 
-move_player(Elements, NewPositionIndex, 2):-
+move_player(Elements, NewPositionIndex, 2, Player1Stones, Player2Stones):-
     /* Move the stone to its new position */ 
     replace(Elements, NewPositionIndex, p2, NewElements),
+    /* Update Player 2 Stones */
+    replace(Player2Stones, NewPositionIndex, p1, NewPlayer2Stones),
 
-    finalize_move(NewElements, CurrPlayer).
-
-finalize_move(Elements, CurrPlayer):-
-    /* Update the board */
-    draw_board(Elements),
-
-    /* Change Player Turn */
-    change_turn_player(Elements, CurrPlayer).
+    finish_player_turn(NewElements, CurrPlayer, NewPositionIndex, NewPlayer1Stones, NewPlayer2Stones).
 
 
-% ------------------- Drop Stone onto the board -----------------------
-
-drop_stone_menu(Elements, CurrPlayer):-
+drop_stone_menu(Elements, CurrPlayer, Player1Stones, Player2Stones):-
 
    /* Insert the stone on the desired position */
 
@@ -202,49 +230,75 @@ drop_stone_menu(Elements, CurrPlayer):-
    /* Verify if position is taken */
    not_taken_rec(Elements, PositionIndex),
 
-   drop_stone_currPlayer(Elements, PositionIndex, CurrPlayer).
+   drop_stone_currPlayer(Elements, PositionIndex, CurrPlayer, Player1Stones, Player2Stones).
 
 
 /* Drops stone depending on the current player */ 
-drop_stone_currPlayer(Elements, PositionIndex, 1):-
+drop_stone_currPlayer(Elements, PositionIndex, 1, Player1Stones, Player2Stones):-
     /* Update the board -> Criar um novo tabuleiro */
    replace(Elements, PositionIndex, p1, NewElements),
-   drop_stone_update(NewElements, 1).
+   /* Update Player Stones */ 
+   append(Player1Stones, [PositionIndex], NewPlayer1Stones),
 
-drop_stone_currPlayer(Elements, PositionIndex, 2):-
+   finish_player_turn(NewElements, 1, PositionIndex, NewPlayer1Stones, Player2Stones).
+
+drop_stone_currPlayer(Elements, PositionIndex, 2, Player1Stones, Player2Stones):-
    /* Update the board -> Criar um novo tabuleiro */
    replace(Elements, PositionIndex, p2, NewElements),
-   drop_stone_update(NewElements, 2).
+   /* Update Player Stones */ 
+   append(Player2Stones, [PositionIndex], NewPlayer2Stones),
 
-/* Finishes the drop stone action */ 
-drop_stone_update(Elements, CurrPlayer):-
+   finish_player_turn(NewElements, 2, PositionIndex, Player1Stones, NewPlayer2Stones).
+
+/* Finishes the player turn */ 
+finish_player_turn(Elements, CurrPlayer, LastPlayedIndex, Player1Stones, Player2Stones):-
    /* Print the new board */
    draw_board(Elements),
 
    /* Change Player Turn */
-   change_turn_player(Elements, CurrPlayer).
-
+   change_turn_player(Elements, CurrPlayer, LastPlayedIndex, Player1Stones, Player2Stones).
 
 % ------------------- Change Player Turn -----------------------
 
-change_turn_player(Elements, 1):-
+change_turn_player(Elements, 1, LastPlayedIndex, Player1Stones, Player2Stones):-
     /* Player 1 */
     NewPlayer is 1+1, 
-    player_turn(Elements, NewPlayer).
+    player_turn(Elements, NewPlayer, LastPlayedIndex, Player1Stones, Player2Stones).
 
 
-change_turn_player(Elements, 2):-
+change_turn_player(Elements, 2, LastPlayedIndex, Player1Stones, Player2Stones):-
     /* Player 2 */
     NewPlayer is 2-1, 
-    player_turn(Elements, NewPlayer).
+    player_turn(Elements, NewPlayer, LastPlayedIndex, Player1Stones, Player2Stones).
+
+% ------------------- Verify if the stone that the player wants to move belongs to him -----------------------
+
+belongs_to_player(1, PositionIndex, Player1Stones, Player2Stones):- member(PositionIndex, Player1Stones).
+
+belongs_to_player(2, PositionIndex, Player1Stones, Player2Stones):- member(PositionIndex, Player2Stones).
 
 
-% ------------------- Verify if the stone belongs to the player (TODO WHEN PlayerVSPlayer Starts to be implemented) -----------------------
-belongs_to_player(player, PlayerName).
+% ------------------- Verify if the inserted or moved stone on the previous play is nearby any of the current player stones -----------------------
 
+verify_inserted_nearby_player(1, LastInsertedIndex, Player1Stones, Player2Stones):-
+    (Up is LastInsertedIndex-4, member(Up,Player1Stones));
+    (Down is LastInsertedIndex+4, member(Down,Player1Stones));
+    (Right is LastInsertedIndex+1, member(Right,Player1Stones));
+    (Left is LastInsertedIndex+1, member(Left,Player1Stones));
+    (RightUp is LastInsertedIndex-3, member(RightUp,Player1Stones));
+    (RightDown is LastInsertedIndex+5, member(RightDown,Player1Stones));
+    (LeftUp is LastInsertedIndex-5, member(LeftUp,Player1Stones));
+    (LeftDown is LastInsertedIndex+3, member(LeftDown,Player1Stones)).
 
-belongs_to_player_rec([H|T], 0, PlayerName) :- belongs_to_player(H, PlayerName).
-belongs_to_player_rec([H|T], Index, PlayerName) :- Index > -1,  I1 is Index-1, belongs_to_player_rec(T, I1, PlayerName).
+verify_inserted_nearby_player(2, LastInsertedIndex, Player1Stones, Player2Stones):-
+    (Up is LastInsertedIndex-4, member(Up,Player2Stones));
+    (Down is LastInsertedIndex+4, member(Down,Player2Stones));
+    (Right is LastInsertedIndex+1, member(Right,Player2Stones));
+    (Left is LastInsertedIndex+1, member(Left,Player2Stones));
+    (RightUp is LastInsertedIndex-3, member(RightUp,Player2Stones));
+    (RightDown is LastInsertedIndex+5, member(RightDown,Player2Stones));
+    (LeftUp is LastInsertedIndex-5, member(LeftUp,Player2Stones));
+    (LeftDown is LastInsertedIndex+3, member(LeftDown,Player2Stones)).
 
 % ------------------- Verify if stone cannot move diagonally  -----------------------
 can_move_diagonally(PositionIndex, Accomulator):-
@@ -278,6 +332,7 @@ not_taken(0).
 not_taken_rec([H|T], 0) :- not_taken(H).
 not_taken_rec([H|T], Index) :- Index > -1,  I1 is Index-1, not_taken_rec(T, I1).
 
+
 % ---------------- Replace Rule ----------------------------
 % replace(+List,+Index,+Value,-NewList). 
 replace([_|T], 0, X, [X|T]).
@@ -288,5 +343,5 @@ replace(L, _, _, L).
 
 game:- board([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],Board),
        get_elements(Board,Elements),
-       draw_board(Elements),
-       player_turn(Elements, 1).
+       draw_board(Elements),       
+       player_turn(Elements, 1, 0, [], []).
